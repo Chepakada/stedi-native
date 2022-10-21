4d765214c67fba5c801e56bde8dcfa96410436ee
chepakada <chepakada@byui.edu>
2022-10-21 10:26:45 -0600

    Student update

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState, } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, Button, TextInput  } from 'react-native';
+import  Navigation from './components/Navigation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from './screens/OnboardingScreen';
+import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+// import { Button, TextInput } from 'react-native-paper';
+
+
+
+
+
+const AppStack = createNativeStackNavigator();
+
+const App = () =>{
+  const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [isLoggedIn,setIsLoggedIn] = React.useState(false);
+  const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+
+   if (isFirstLaunch == true){
+return(
+  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
+ 
+);
+  }else if(isLoggedIn){
+    return <Navigation/>
+  }
+  else{
+    return(
+      <View>
+        <TextInput style={styles.input}
+        placeholderTextColor="#4251F5"
+        placeholder= 'Phone Number'></TextInput>
+        <Button 
+        title = 'Send'
+        style = {styles.button}
+        onPress = {()=>{
+          console.log('Button was pressed')
+        }}/>
+      </View>
+    )
+  }
+}
+ export default App;
+ const styles = StyleSheet.created({
+ container:{
+  flex:1, 
+  alignItems:'center',
+  justifyContent: 'center'
+},
+input: {
+  marginTop:100,
+height: 40,
+margin: 12,
+borderWidth: 1,
+padding: 10,
+},
+margin:{
+marginTop:100
+},
+button: {
+alignItems: "center",
+backgroundColor: "#DDDDDD",
+padding: 10
+}    
+})
