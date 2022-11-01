330df83c5746cc3396d74d4ce63bcd05f3232e7a
chepakada <chepakada@byui.edu>
2022-11-01 14:20:18 -0600

    phone text login

--- App.js
+++ App.js
@@ -17,33 +17,84 @@ const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
   const [isLoggedIn,setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [phoneNumber, setPhoneNumber]= React.useState("");
 
    if (isFirstLaunch == true){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else if(isLoggedIn){
+  } else if(loggedInState == loggedInStates.LOGGED_IN){
     return <Navigation/>
   }
-  else{
+ else if(loggedInState == loggedInStates.NOT_LOGGED_IN){
     return(
       <View>
         <TextInput style={styles.input}
         placeholderTextColor="#4251F5"
-        placeholder= 'Phone Number'></TextInput>
+        placeholder= 'Phone Number'
+        value= {phoneNumber}
+        onChangeText= {setPhoneNumber}></TextInput>
+        
+        
         <Button 
         title = 'Send'
         style = {styles.button}
-        onPress = {()=>{
+        onPress = {async()=>{
           console.log('Button was pressed')
-        }}/>
+        
+          await fetch('https://dev.stedi.me/twofactorlogin/+phoneNumber',
+        {
+          method: 'POST',
+          headers:{
+            'content-type':'application/text'
+         
+          }
+          
+        }
+          )
+          setLoggedInState(loggedInStates.CODE_SENT)
+          }}
+        />
+      </View>
+    )}
+    else if (loggedInState == loggedInStates.CODE_SENT){
+      return(
+      <View>
+        <TextInput 
+          placeholder='One Time Password'
+          style={styles.input}
+          placeholderTextColor='#4251f5'
+          value={oneTimePassword}
+          onChangeText={setOneTimePassword}
+          keyboardType = "numeric"
+        >
+        </TextInput>
+        <Button
+        title='Login'
+          style={styles.button}
+          onPress={async()=>{
+            console.log('Login Button was pressed!')
+            const loginResponse=await fetch ('https://dev.stedi.me/twofactorlogin',
+            {
+              method:'POST',
+              headers:{
+                'content-type':'application/text'
+              },
+              body: phoneNumber, oneTimePassword
+            })
+            setLoggedInState(loggedInStates.CODE_SENT)
+          }}
+        />
       </View>
     )
-  }
-}
- export default App;
- const styles = StyleSheet.created({
+  }}
+        
+    
+
+     
+  export default App;
+ const styles = StyleSheet.create({
  container:{
   flex:1, 
   alignItems:'center',
@@ -53,15 +104,6 @@ input: {
   marginTop:100,
 height: 40,
 margin: 12,
-borderWidth: 1,
-padding: 10,
-},
-margin:{
-marginTop:100
-},
-button: {
-alignItems: "center",
-backgroundColor: "#DDDDDD",
-padding: 10
-}    
-})
+borderWidth: 1
+}
+ })
